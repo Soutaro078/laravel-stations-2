91ced079f96b6c42e2a8d86885fa7e8f447ec465
Soutaro078 <kkrt235kw@gmail.com>
2025-02-10 09:54:24 +0900

    Station5まで完了

---
+++ app/Http/Controllers/PracticeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use App\Models\Practice; // ここに追加
+
+class PracticeController extends Controller
+{
+    public function sample(){
+
+      return view('practice');;
+    }
+
+    public function sample2(){
+
+        $test = 'practice2';
+            return view('practice2', ['testParam' => $test]);
+    }
+
+    public function sample3(){
+            
+        $test = 'test';
+            return view('practice3', ['testParam' => $test]);
+    }
+
+    public function getPractice(){
+
+        $practice = Practice::all();
+        return response()->json($practice);    
+    }
+
+}
